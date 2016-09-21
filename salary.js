5ab9121cb873c88ebd587afaf702b8dde5eb737f
MattYockey <yockey_matt@yahoo.com>
2016-09-21 14:52:38 -0400

    Add files via upload

---
+++ salary.js
@@ -0,0 +1,23 @@
+/**
+ *   @author Matt, Howard (yockey_matt@yahoo.com)
+ *   @version 0.0.3
+ *   @summary Project 2 demo code || created: 03.16.2016
+ *   @todo
+ */
+
+function baseSalary() {
+    baseSalary = PROMPT.question(`\nenter base totalSalary: `);
+}
+
+function totalSales() {
+   totalSale = PROMPT.question(`\nenter base totalSales: `);
+}
+
+function commissionRate() {
+    comminssionRate = PROMPT.question(`\nenter base commissionRate: `);
+}
+
+function totalSalary() {
+    totalSalay = PROMPT.question(`\nenter base totalSalary: `);
+}
+
